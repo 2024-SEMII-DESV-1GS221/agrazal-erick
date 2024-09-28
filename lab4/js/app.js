c59dd1d521376effc0a8a15cc022a937c3ddf7b9
Erick Vicente Agrazal Lopez <erick@agrazal.com>
2024-09-27 19:30:23 -0500

    Adding lab 4 changes

--- lab4/js/app.js
+++ lab4/js/app.js
@@ -3,13 +3,32 @@ const App = (({ reverseString }) => {
     form: document.querySelector('form'),
     response: document.querySelector('#response'),
   }
+  const templates = {
+    response: c => {
+      const color = htmlElements.form.elements.color.value;
+      const colorMap = {
+        white: 'white',
+        blue: 'blue',
+        grey: 'grey',
+      }
+      return `<span class="color-${colorMap[color]}">${c}</span>`;
+    }
+  }
   const handlers = {
     onInputChange(e) {
-      htmlElements.response.textContent = reverseString(e.target.value);
+      const response = reverseString(e.target.value);
+      const template = templates.response(response)
+      htmlElements.response.innerHTML = template;
+    },
+    onColorChange(e) {
+      const response = reverseString(htmlElements.form.cadena.value);
+      const template = templates.response(response)
+      htmlElements.response.innerHTML = template;
     }
   }
   const bindEvents = () => {
     htmlElements.form.elements.cadena.addEventListener('input', handlers.onInputChange);
+    htmlElements.form.elements.color.addEventListener('change', handlers.onColorChange);
   }
   return {
     init() {
